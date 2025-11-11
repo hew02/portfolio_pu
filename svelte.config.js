072ca9be4b7107c97919dd4f79adc612966790ed
Juan Diego Mora Rubio <juanjuo37@gmail.com>
2025-11-11 11:41:53 -0600

    github server update

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? ' ' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? 'src' : process.env.BASE_PATH
 			//base: <a href="{base}/portfolio_pu">Link</a>
 		}
 	}
